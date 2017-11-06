b12e4bf7a568096f6568beb92713e3c55d9cb010
Nikita <dev@zh-ar.ru>
2017-11-06 23:20:48 +0400

    gulp i

--- RosFi/gulpfile.js
+++ RosFi/gulpfile.js
@@ -28,7 +28,8 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.min.js', // Всегда в конце
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/detect.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
 	// .pipe(uglify()) // Минимизировать весь js (на выбор)
